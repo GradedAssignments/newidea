c3fc4032418ce9021514bb97b410279fd686a90d
IIT Madras Students Community <142117059+GradedAssignments@users.noreply.github.com>
2023-09-08 14:44:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,17 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
         [flowDocs, flowGracias, flowTuto, flowDiscord, flowNotes]
     )
 
-
+const flowPrincipal = addKeyword(['Iitmadras',])
+    .addAnswer('🙌 Hola bhai Aur Sab Badiya kya Bro bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *doc* para ver la documentación',
+            '👉 *gracias*  para ver la lista de videos',
+            '👉 *discord* unirte al discord',
+        ],
+        null,
+        null,
 
 const main = async () => {
     const adapterDB = new MockAdapter()
@@ -90,5 +100,6 @@ const main = async () => {
 
     QRPortalWeb()
 }
+)
 
 main()
